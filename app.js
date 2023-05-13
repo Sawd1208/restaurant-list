c64b7f7fd033a1675ac0c4bb9ff5c6fa09699108
sawd1208 <sawdchien@gmail.com>
2023-05-13 13:58:08 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,29 +1,13 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
-// 僅在非正式環境時，使用dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-const Restaurant = require('./models/Restaurant')
 const routes = require('./routes')
+require('./config/mongoose')
+
 const app = express()
 const port = 3000
-
-mongoose.connect(process.env.MONGODB_URI) 
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongoose error!')
-})
-
-db.once('open', () => {
-  console.log('mongoose connected!')
-})
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -2,16 +2,8 @@ const mongoose = require('mongoose')
 const Restaurant = require('../Restaurant')
 const restaurantList = require('../../restaurant.json').results
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
+const db = require('../../config/mongoose')
 
-mongoose.connect(process.env.MONGODB_URI)
-
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
 db.once('open', () => {
   console.log('mongodb connected!')
   Restaurant.create(restaurantList)
