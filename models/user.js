05dfb99a4f11c49324be1afcdcbb75c27d44bbfd
sawd1208 <sawdchien@gmail.com>
2023-06-13 16:37:01 +0800

    add user registration feature

--- models/user.js
+++ models/user.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const userSchema = new Schema({
-  name:{ tyep: String, required: true},
-  email: { tyep: String, required: true },
-  password: { tyep: String, required: true },
-  createAt: { tyep: Date, default: Date.now }
+  name:{ type: String, required: true},
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  createAt: { type: Date, default: Date.now }
 })
 
-module.exports = mongoose.model(User, userSchema)
+module.exports = mongoose.model('User', userSchema)
