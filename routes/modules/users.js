1f0fec339757bf8b03a7a942c84c9db4af5f25ba
sawd1208 <sawdchien@gmail.com>
2023-06-15 22:19:56 +0800

    add message partials

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -10,7 +10,8 @@ router.get('/login', (req, res) => {
 router.post('/login', passport.authenticate('local', {
   // 加入 middleware，驗證 request 登入狀態
   successRedirect: '/',
-  failureRedirect: '/user/login'
+  failureRedirect: '/users/login',
+  failureFlash: true,
 }))
   
 router.get('/register', (req, res) => {
@@ -19,11 +20,28 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填。'})
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不符合！'})
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
   User.findOne({ email })
     .then(user => {
       if (user) {
-        console.log('User already exist.')
+        errors.push({ message: '這個 Email 已經註冊過了。'})
         res.render('register', {
+          errors,
           name,
           email,
           password,
@@ -44,6 +62,7 @@ router.post('/register', (req, res) => {
 
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '你已經成功登出。')
   res.redirect('/users/login')
 })
 
