89dafe3d2622d9e4f6a0dbcb250a63dfcb41849a
sawd1208 <sawdchien@gmail.com>
2023-06-29 22:21:37 +0800

    modify

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -36,31 +36,3 @@ db.once('open', () => {
     process.exit()
   })
 })
-
-
-
-
-// db.once('open', () => {
-//   Promise.all(seedUsers.map((seedUser) =>
-//     bcrypt
-//       .genSalt(10)
-//       .then(salt => bcrypt.hash(seedUser.password, salt))
-//       .then(hash => User.create({
-//         name: seedUser.name,
-//         email: seedUser.email,
-//         password: hash
-//       }))
-//       .then(user => {
-//         const userId = user._id
-//         const restaurant = seedUser.item.map(index => {
-//           restaurantList[index].userId = userId
-//           return restaurantList[index]
-//         })
-//         return Restaurant.create(restaurant)
-//       })
-//   ))
-//     .then(() => {
-//       console.log('done')
-//       process.exit()
-//     })
-// })
